0820dcff4a8daa68dc7977d92d62f5c185a3c31f
Omari-Elizabeth <elizabeth.omari@student.moringaschool.com>
2022-10-21 03:53:11 +0300

    commit tree d991130d6927d75eaaa1768d3cd003750e3d8bf6

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre, :turn_page
+
+    def initialize(title)
+        @title = title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+
+# current_read = Book.new("And Then There Were None")
+# current_read.author_name = "Agatha Christie"
+# current_read.page_count = 272
+# current_read.genre = "Mystery"
 
 
+# puts current_read
